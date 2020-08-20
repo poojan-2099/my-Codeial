2975836bb0ad358cc08dd8694a2638e69cb11adc
poojan2099 <41160130+poojan2099@users.noreply.github.com>
2020-08-20 17:05:24 +0530

    set up static files access

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express=require('express');
 const app=express();
 const port=8000;
+const expressLayouts=require('express-ejs-layouts');
+
+app.use(express.static('./assets'))
+//we have to put layouts first before routes
+app.use(expressLayouts);
+//extract style and scripts from seb pages into the layout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true)
 
 //use express router
 app.use('/',require('./routes'))
 //set up the view engine
-app.use('view engine','ejs');
+app.set('view engine','ejs');
 app.set('views','./views');
 
 app.listen(port,function(err){
