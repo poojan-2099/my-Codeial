e2556ea97fa89d7f65c15e7da075dc73ef4097c5
poojan2099 <41160130+poojan2099@users.noreply.github.com>
2020-08-20 23:21:51 +0530

    added cookies

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -4,14 +4,26 @@ module.exports.profile=function(req,res){
     })
 }
 
+//render the signup page
 module.exports.signUp=function(req,res){
     return res.render('user_sign_up',{
         Title:"Codeial | Sign Up"
     })
 }
 
+//render the signin page
 module.exports.signIn=function(req,res){
     return res.render('user_sign_in',{
         Title:"Codeial | Sign In"
     })
+}
+
+//get the signUp data
+module.exports.create=function(req,res){
+    //TODo later
+}
+
+//sign in and create a session for user
+module.exports.createSession=function(req,res){
+    //TODo later
 }
