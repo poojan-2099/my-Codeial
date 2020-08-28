11b6bd503b0a197dae920cf8186ca7e1cbfa2f1c
poojan2099 <41160130+poojan2099@users.noreply.github.com>
2020-08-28 12:04:48 +0530

    Error handling and async

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,30 @@
-const Post=require('../models/post');
-const User=require('../models/user')
+const Post = require('../models/post');
+const User = require('../models/user')
 
-module.exports.home=function(req,res){
+module.exports.home = async function (req, res) {
+    try {
+        //populate the user of each post
+        let posts = await Post.find({})
+            .populate('user')
+            .populate({
+                path: 'comments',
+                populate: {
+                    path: 'user'
+                }
+            });
+
+        let users = await User.find({});
+
+        return res.render('home', {
+            Title: 'Codeial | Home',
+            posts: posts,
+            all_users: users
+        });
+
+    } catch (err) {
+        console.log('ERror',err);
+        return;
+    }
     // console.log(req.cookies);
     // res.cookie('user_id',25)
     // Post.find({},function(err,posts){
@@ -11,29 +34,6 @@ module.exports.home=function(req,res){
     //     });  
     // });
 
-    //populate the user of each post
-    Post.find({})
-    .populate('user')
-    .populate({
-        path:'comments',
-        populate:{
-            path:'user'
-        }
-    })
-    .exec(function(err,posts){
-
-        User.find({},function(err,users){
-            return res.render('home',{
-                Title:'Codeial | Home',
-                posts:posts,
-                all_users:users
-            }); 
-        });
-
-        
-    })
-
-   
 }
 
 //module.exports.actionname 
