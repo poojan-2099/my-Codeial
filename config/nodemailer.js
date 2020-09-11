6f7ae32bc25a6c250f5997c55a97e8be5b5d7456
poojan2099 <41160130+poojan2099@users.noreply.github.com>
2020-09-11 18:38:54 +0530

    setting and config nodemailer

---
+++ config/nodemailer.js
@@ -0,0 +1,36 @@
+const nodeMailer=require('nodemailer');
+const ejs=require('ejs');
+const path=require('path');
+
+
+
+let transporter=nodeMailer.createTransport({
+    service:'gmail',
+    host:'smtp.gmail.com',
+    port:587,
+    secure:false,
+    auth:{
+        user:'ppatel2099@gmail.com',
+        pass:'8347961590'
+    }
+
+});
+
+let renderTemplate=(data,relativePath)=>{
+    let mailHTML;
+    ejs.renderFile(
+        path.join(__dirname,'../views/mailers',relativePath),
+        data,
+        function(err,template){
+            if(err){console.log('Error in rendering Template',err); return;}
+
+            mailHTML=template;
+        }
+    )
+    return mailHTML;
+}
+
+module.exports={
+    transporter:transporter,
+    renderTemplate:renderTemplate
+}
